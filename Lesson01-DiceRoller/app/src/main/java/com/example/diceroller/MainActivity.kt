f30df30df939431913e4936cec335677d5940a35
blood <hey-blood@foxmail.com>
2021-06-22 00:01:18 +0800

    Dice Images: Adding the ImageView

--- Lesson01-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Lesson01-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -20,8 +20,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
