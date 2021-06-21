6ab9b1feb2cea5a288181a8f43de39230637eda1
blood <hey-blood@foxmail.com>
2021-06-21 23:49:06 +0800

    Connecting the Button: OnClickListener

--- Lesson01-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Lesson01-DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,9 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
+
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
