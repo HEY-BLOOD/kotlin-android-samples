506ac36986eb476ea33a0c1fee059b8be9cf42a7
blood <hey-blood@foxmail.com>
2021-07-01 09:45:55 +0800

    Add Support for the Up Button

--- Lesson03-AndroidTrivia/app/src/main/java/com/example/androidtrivia/MainActivity.kt
+++ Lesson03-AndroidTrivia/app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.androidtrivia
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         @Suppress("UNUSED_VARIABLE")
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // Since we're using KTX, you can call this.this.findNavController()
+        val navController = this.findNavController(R.id.navHostFragment)
+        // Link the navController to our ActionBar
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
