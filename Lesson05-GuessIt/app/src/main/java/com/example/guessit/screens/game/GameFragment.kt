92caba557e6c4d8c7044720fd7a89cf7f5c9020f
blood <hey-blood@foxmail.com>
2021-07-16 00:29:12 +0800

    Create the GameViewModel

--- Lesson05-GuessIt/app/src/main/java/com/example/guessit/screens/game/GameFragment.kt
+++ Lesson05-GuessIt/app/src/main/java/com/example/guessit/screens/game/GameFragment.kt
@@ -1,11 +1,13 @@
 package com.example.guessit.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.guessit.R
 import com.example.guessit.databinding.FragmentGameBinding
@@ -14,6 +16,9 @@ import com.example.guessit.databinding.FragmentGameBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+
+    // Lazy initialization
+    private val viewModel: GameViewModel by viewModels()
 
     // The current word
     private var word = ""
@@ -38,6 +43,9 @@ class GameFragment : Fragment() {
             container,
             false
         )
+
+        // Call the function of ViewModel so that it is actually created
+        Log.i("GameViewModel", viewModel.hashCode().toString())
 
         resetList()
         nextWord()
