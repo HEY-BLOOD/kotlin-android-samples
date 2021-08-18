67f34cf92bc537a8ecd36b6b8a73570c3cfa29bc
blood <hey-blood@foxmail.com>
2021-08-18 08:04:29 +0800

    Refresh Data with DiffUtil

--- Lesson07-RecyclerSleepTracker/app/src/main/java/online/zhenhong/recyclersleeptracker/sleeptracker/SleepNightAdapter.kt
+++ Lesson07-RecyclerSleepTracker/app/src/main/java/online/zhenhong/recyclersleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -21,24 +21,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import online.zhenhong.recyclersleeptracker.R
 import online.zhenhong.recyclersleeptracker.convertDurationToFormatted
 import online.zhenhong.recyclersleeptracker.convertNumericQualityToString
 import online.zhenhong.recyclersleeptracker.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
-
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun getItemCount() = data.size
+class SleepNightAdapter :
+    ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
 
         holder.bind(item)
     }
@@ -80,5 +75,20 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 return ViewHolder(view)
             }
         }
+    }
+
+    /**
+     * Callback for calculating the diff between two non-null items in a list.
+     *
+     * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
+     * list that's been passed to `submitList`.
+     */
+    object SleepNightDiffCallback :
+        DiffUtil.ItemCallback<SleepNight>() {
+        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean =
+            oldItem.nightId == newItem.nightId
+
+        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean =
+            oldItem == newItem
     }
 }
--- Lesson07-RecyclerSleepTracker/app/src/main/java/online/zhenhong/recyclersleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ Lesson07-RecyclerSleepTracker/app/src/main/java/online/zhenhong/recyclersleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -25,10 +25,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import online.zhenhong.recyclersleeptracker.R
 import online.zhenhong.recyclersleeptracker.database.SleepDatabase
 import online.zhenhong.recyclersleeptracker.databinding.FragmentSleepTrackerBinding
-import com.google.android.material.snackbar.Snackbar
 
 /**
  * A fragment with buttons to record start and end times for sleep, which are saved in
@@ -71,7 +71,7 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepRecyclerView.adapter = adapter
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
