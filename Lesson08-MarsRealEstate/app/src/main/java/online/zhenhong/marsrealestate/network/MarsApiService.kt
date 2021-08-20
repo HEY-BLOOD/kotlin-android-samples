83335fa212bbe383ab000dd3e128e42e5c9672ab
blood <hey-blood@foxmail.com>
2021-08-20 18:30:18 +0800

    Parsing the JSON Response - With Moshi

--- Lesson08-MarsRealEstate/app/src/main/java/online/zhenhong/marsrealestate/network/MarsApiService.kt
+++ Lesson08-MarsRealEstate/app/src/main/java/online/zhenhong/marsrealestate/network/MarsApiService.kt
@@ -17,19 +17,29 @@
 
 package online.zhenhong.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
 /**
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
  * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
- * object pointing to the desired URL
+ * object.
  */
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
@@ -44,8 +54,7 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("realestate")
-    fun getProperties():
-            Call<String>
+    fun getProperties(): Call<List<MarsProperty>>
 }
 
 /**
