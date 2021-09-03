67d467e206b33bc6f24f8df549080664c2edd055
blood <hey-blood@foxmail.com>
2021-09-03 23:33:02 +0800

    Create a Worker

--- Lesson09-DevByteViewer/app/src/main/java/online/zhenhong/devbyteviewer/work/RefreshDataWork.kt
+++ Lesson09-DevByteViewer/app/src/main/java/online/zhenhong/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,28 @@
  */
 
 package online.zhenhong.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import online.zhenhong.devbyteviewer.database.getDatabase
+import online.zhenhong.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    /**
+     * A coroutine-friendly method to do your work.
+     */
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
