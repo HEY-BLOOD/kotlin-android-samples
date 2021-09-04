0353f3704eee6d8214c840e292aa542fa9e34ca3
blood <hey-blood@foxmail.com>
2021-09-04 15:26:48 +0800

    Add starter code

---
+++ Lesson10-GDGFinder/app/src/main/java/online/zhenhong/gdgfinder/network/GdgApiService.kt
@@ -0,0 +1,30 @@
+package online.zhenhong.gdgfinder.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+interface GdgApiService {
+    @GET("gdg-directory.json")
+    suspend fun getChapters(): Response<GdgResponse>
+}
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+object GdgApi {
+    val retrofitService: GdgApiService by lazy { retrofit.create(GdgApiService::class.java) }
+}
